8deadb3b58df6cc22cfabc0ef39f2dcf0e1a0d48
dev3style <hungpt58.uet@gmail.com>
2021-12-20 16:04:27 +0700

    Fix wrong validation messages for user model

--- src/main/scala/com/reactive/pizza/models/user/User.scala
+++ src/main/scala/com/reactive/pizza/models/user/User.scala
@@ -8,8 +8,8 @@ case class User(
 ) {
   //------------[ Validations ]-----------------
   require(1 <= email.length && email.length <= 255,       "Email must be from 1 to 255 characters")
-  require(1 <= username.length && username.length <= 255, "Email must be from 1 to 255 characters")
-  require(1 <= password.length && password.length <= 255, "Email must be from 1 to 255 characters")
+  require(1 <= username.length && username.length <= 255, "Username must be from 1 to 255 characters")
+  require(1 <= password.length && password.length <= 255, "Password must be from 1 to 255 characters")
 }
 
 object User {
